92b6831ef1b17ddcb3ec57b26fcbb5b7e4d1a017
danzok <loppesofc@gmail.com>
2022-11-10 23:15:59 -0300

    factory code auth and import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import  makeWASocket, { DisconnectReason } from '@adiwajshing/baileys';
+import  makeWASocket, {Browsers, DisconnectReason, useSingleFileLegacyAuthState } from '@adiwajshing/baileys';
+const { state, saveState } = await useSingleFileLegacyAuthState('./src/cache/auth_session-zap.json')
+import * as fs from 'fs';
 
 async function connectzap(){
-    const sock = makeWASocket.default({
-
-        printQRInTerminal: true
+    const conn = makeWASocket.default({
+        browser: Browsers.ubuntu('Desktop'),
+        auth: state,
+        printQRInTerminal: true,
+        browser: ['@danzok','chrome','1.0.0']
     })
 
+    conn.ev.on('creds.update', saveState)
+    console.log(saveState)
 }
 
 connectzap()
