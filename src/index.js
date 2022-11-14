7be302f2793157b33be91be61c79426ce3f5ca4f
danzok <loppesofc@gmail.com>
2022-11-14 00:37:13 -0300

    add try error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import  makeWASocket, {Browsers, useMultiFileAuthState , DisconnectReason} from '@adiwajshing/baileys';
 const { state, saveCreds } = await useMultiFileAuthState('./src/cache_session')
 import { Boom } from '@hapi/boom';
+import P from 'pino';
 
 async function connectzap(){
     const conn = makeWASocket.default({
+        logger: P({ level: 'silent' }),
         browser: Browsers.ubuntu('Desktop'),
         auth: state,
         printQRInTerminal: true,
         browser: ['@danzok','chrome','1.0.0']
     })
 
+    console.log(state)
+    conn.ev.on('creds.update', saveCreds)
     conn.ev.on('connection.update', (update) => {
         const { connection , lastDisconnect } = update;
 
@@ -24,13 +28,16 @@ async function connectzap(){
         } else if(connection === 'open') {
             console.log('opened connection')
         }
-
-        conn.ev.on('creds.update', saveCreds)
     })
 
      conn.ev.on('messages.upsert', async (m)  =>{
-        const fromId = m.messages[0].key.remoteJid
-        await conn.sendMessage(fromId, { text: 'Hello there!' })
+        try {
+            const fromId = m.messages[0].key.remoteJid
+            await conn.sendMessage(fromId, { text: 'Hello there!' })
+        } catch (error) {
+            console.log(error)
+        }
      })
+
 }
 connectzap()
