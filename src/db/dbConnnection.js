5241a8ce1a68ca8f09e54b53131faa3e116a29d2
danzok <105066526+danzok@users.noreply.github.com>
2022-11-17 17:19:04 -0300

    Update dbConnnection.js

--- src/db/dbConnnection.js
+++ src/db/dbConnnection.js
@@ -1,7 +1,7 @@
 ﻿import { MongoClient } from "mongodb";
 
 
-let mongoClient = new MongoClient('mongodb+srv://Lopes01:Lopes01@cluster0.evxl2c8.mongodb.net/?retryWrites=true&w=majority');
+let mongoClient = new MongoClient('mongodb+srv://your:your@cluster0.evxl2c8.mongodb.net/?retryWrites=true&w=majority');
 mongoClient.connect().then(()=>{
     console.log('connected your database')
 })
