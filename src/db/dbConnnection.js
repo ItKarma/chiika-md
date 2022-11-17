f93be4253ce9f18630c7c5da380b22752449a037
danzok <loppesofc@gmail.com>
2022-11-17 12:14:52 -0300

    feat : db conncetion

---
+++ src/db/dbConnnection.js
@@ -0,0 +1,14 @@
+﻿import { MongoClient } from "mongodb";
+
+export default async function MongoConnection(uriMongo){
+    if(!uriMongo) throw new Error('uriMongo is required')
+
+    try {
+        const mongoClient = new MongoClient(uriMongo);
+        await mongoClient.connect();
+        console.log(' Connected your database!')
+        return mongoClient;
+    } catch (error) {
+        console.log(error);
+    }
+}
