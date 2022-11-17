2de4cac40a8248fff1df1ff12bf10f38f853236c
danzok <loppesofc@gmail.com>
2022-11-16 22:18:14 -0300

    'config'

---
+++ src/config.js
@@ -0,0 +1,11 @@
+﻿export const config = {
+    "prefix" : "!",
+    "owner" : [
+        // add your number here
+        // you can add multiple numbers
+        "5591984155848@s.whatsapp.net",
+        "98765@s.whatsapp.net"
+    ],
+    "botname" : "chika", // bot name
+    "footer": "chika • chika"
+}
