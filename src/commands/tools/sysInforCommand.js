708d0d2f136f4f18343dee48d312e3e20ab24fee
danzok <loppesofc@gmail.com>
2022-11-18 20:19:16 -0300

    feat: add comand infor sistem category tools

---
+++ src/commands/tools/sysInforCommand.js
@@ -0,0 +1,9 @@
+import os from "node:os"
+
+export default async function sysInforTools (msg) {
+           
+    let str = `*OS:* ${os.type() + " " + os.arch()}\n*Host:* ${os.hostname()}\n*Kernel:* ${os.release()}\n*Uptime:* ${parseInt(os.uptime() / 60)} minutes\n*Shell:* ${os.userInfo().shell}\n*Terminal:* ${process.env.TERM}\n*CPU:* ${os.cpus().map((cpu) => cpu.model)}\n*Free Memory:* ${parseInt(os.freemem() / 1e+6)}MB\n`
+
+    return await msg.reply(str)
+
+}
