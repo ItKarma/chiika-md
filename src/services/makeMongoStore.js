090b7ade00363b9380e94bca887c2eed1bbdcb7f
danzok <loppesofc@gmail.com>
2022-11-18 13:31:30 -0300

    salvando apenas as messages dos usuarios

--- src/services/makeMongoStore.js
+++ src/services/makeMongoStore.js
@@ -5,7 +5,8 @@ export const makeMongoStore = async (messagesCollection) => ({
   bind: (ev) => {
     ev.on('messages.upsert', async ({ messages, type }) => {
       if (type !== 'notify') return
-
+      if(!messages[0]) return 
+      
       await messagesCollection.insertMany(
         messages.map(({ message, ...rest }) => ({
           ...rest,
