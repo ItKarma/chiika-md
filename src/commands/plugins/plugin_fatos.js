acae75b4e43045e39f4b4aa5ad9724f210c7de0b
danzok <loppesofc@gmail.com>
2022-11-19 18:55:50 -0300

    refactory plugin fatos

--- src/commands/plugins/plugin_fatos.js
+++ src/commands/plugins/plugin_fatos.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import { config } from "../../config.js";
 
 
-export default async function fatosPlugin (from, conn) {
+export default async function fatosPlugin (msg, conn) {
     let { data } = await axios.get('https://apis-geek.vercel.app/fatosRandom');
     let buttons = [
         { quickReplyButton: { displayText: "menu ☰ ", id: `menu` } },
         { quickReplyButton: { displayText: "proximo ➡️", id: `fatos` } }
     ];
 
-    return await conn.sendMessage(from, {
+    return await conn.sendMessage(msg.from, {
         text : data.data,
         templateButtons: buttons,
         footer: config.footer
