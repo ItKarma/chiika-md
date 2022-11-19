c6cb37bc0a2f7bebf21520e5bf24c7b65124b65d
danzok <loppesofc@gmail.com>
2022-11-19 14:58:43 -0300

    detele factRnadom , update from plugin factos, add path plugin

---
+++ src/commands/plugins/plugin_fatos.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { config } from "../../config.js";
+
+
+export default async function fatosPlugin (from, conn) {
+    let { data } = await axios.get('https://apis-geek.vercel.app/fatosRandom');
+    let buttons = [
+        { quickReplyButton: { displayText: "menu ☰ ", id: `menu` } },
+        { quickReplyButton: { displayText: "proximo ➡️", id: `fatos` } }
+    ];
+
+    return await conn.sendMessage(from, {
+        text : data.data,
+        templateButtons: buttons,
+        footer: config.footer
+    }, { quoted : msg });
+}
