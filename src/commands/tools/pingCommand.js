0d7f1c713aa308882163f7bd64683d4cbf97d6f3
danzok <loppesofc@gmail.com>
2022-11-19 19:00:21 -0300

    nothing

--- src/commands/tools/pingCommand.js
+++ src/commands/tools/pingCommand.js
@@ -1,4 +1,4 @@
-import pingUtils from "../../utils/ping_utils.js";
+import pingUtils from "../../utils/ping_Utils.js";
 
 export default function pingTools (msg) {
     msg.reply(`*PONG!*\n*_${pingUtils(msg.messageTimestamp, Date.now())} second(s)_*`);
