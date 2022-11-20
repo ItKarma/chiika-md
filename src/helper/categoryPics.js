09ab6e17a9af68f51df63c960efe06671961404a
danzok <loppesofc@gmail.com>
2022-11-20 00:55:22 -0300

    feat : add category de pics

---
+++ src/helper/categoryPics.js
@@ -0,0 +1,5 @@
+let categoryArray = new Array("waifu", "neko","shinobu","megumin","bully","cuddle","cry","hug","awoo",
+"kiss","lick","pat","smug","bonk","yeet","blush","smile","wave","highfive","handhold","nom","bite",
+"glomp","slap","kill","kick","happy","wink","peoke","dance","cringe")
+
+export default categoryArray;
