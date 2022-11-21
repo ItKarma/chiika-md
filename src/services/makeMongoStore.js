1e83868975eba45a02cdff8287b78058e3e626eb
danzok <loppesofc@gmail.com>
2022-11-20 23:30:42 -0300

    update : salving number and name from user

--- src/services/makeMongoStore.js
+++ src/services/makeMongoStore.js
@@ -1,18 +1,12 @@
-﻿
-import { BufferJSON } from "@adiwajshing/baileys";
+﻿export const makeMongoStore = async (messagesCollection) => ({
+  bind: async (msg) => {
 
-export const makeMongoStore = async (messagesCollection) => ({
-  bind: (ev) => {
-    ev.on('messages.upsert', async ({ messages, type }) => {
-      if (type !== 'notify') return
-      if(!messages[0]) return 
-      
-      await messagesCollection.insertMany(
-        messages.map(({ message, ...rest }) => ({
-          ...rest,
-          message: JSON.stringify(message, BufferJSON.replacer)
-        }))
-      )
-    })
+       let { sender,pushName } = msg
+
+     await messagesCollection.insertMany([ {
+      userID : sender,
+      userName: pushName,
+     }]);
+
   },
 })
