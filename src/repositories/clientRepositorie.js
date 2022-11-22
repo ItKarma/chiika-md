87b60a6cd4024e6c258b6e7338a492b940ddf4b0
danzok <loppesofc@gmail.com>
2022-11-21 21:33:21 -0300

    adicionando a verificaçao se o user estar ou nao salvo

--- src/repositories/clientRepositorie.js
+++ src/repositories/clientRepositorie.js
@@ -10,4 +10,5 @@ let clientList = async () => {
     }
 }
 
+
 export { clientList }
--- src/repositories/makeMongoStore.js
+++ src/repositories/makeMongoStore.js
@@ -1,7 +1,11 @@
-﻿export const makeMongoStore = async (messagesCollection) => ({
+﻿import { messageCollection } from "../services/serviceCollections.js";
+
+export const makeMongoStore = async (messagesCollection) => ({
   bind: async (msg) => {
 
        let { sender,pushName } = msg
+      if(await messageCollection.findOne({ userID : sender })) return 
+       
 
      await messagesCollection.insertMany([ {
       userID : sender,
