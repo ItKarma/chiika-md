d66b1887236a757f066a50ce60672d2afa5cb3e2
danzok <loppesofc@gmail.com>
2022-11-22 20:16:01 -0300

    nothing

--- src/db/dbConnnection.js
+++ src/db/dbConnnection.js
@@ -4,8 +4,7 @@ import 'dotenv/config';
 
 let mongoClient = new MongoClient(process.env.URL_MONGO);
 mongoClient.connect()
-.then(()=>{
-    console.log('connected your database')})
+.then(() => console.log('connected your database'))
     .catch(error => console.log(error));
 
 export default mongoClient;
