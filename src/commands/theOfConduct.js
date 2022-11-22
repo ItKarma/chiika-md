92d4d5ab3a3b1225232a3d58fda237aa42d162f0
danzok <loppesofc@gmail.com>
2022-11-21 21:30:20 -0300

    adicionado o comando de condut para salvar os dados do cliente no banco d dados

---
+++ src/commands/theOfConduct.js
@@ -0,0 +1,23 @@
+import { config } from "../config.js";
+import { messageCollection } from "../services/serviceCollections.js"
+
+export let firstTime = async (msg, conn) => {
+    const { sender , pushName } = msg;
+
+    if(await messageCollection.findOne({ userID : sender })) return 
+
+    let messageTxt = `*Ola ${pushName}*, Percebi que vc nao esta salvo no nosso banco de dados , entao posso salvar seu contato certo? , aperte em concordo , e podera usar meus comandos sem restriçao.`
+
+    const buttons = [
+        {buttonId: '!agree', buttonText: {displayText: 'ACEITO OS TERMOS'}, type: 1},
+      ]
+      
+      const buttonMessage = {
+          text: messageTxt,
+          footer: config.footer,
+          buttons: buttons,
+          headerType: 1
+      } 
+
+     await conn.sendMessage(msg.from, buttonMessage, { quoted : msg})
+}
