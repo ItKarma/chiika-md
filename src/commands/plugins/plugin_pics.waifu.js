186897d455a4dd70a5d78e3eb211089f4ff95003
danzok <loppesofc@gmail.com>
2022-11-22 22:16:59 -0300

    aternando para lib fetch

--- src/commands/plugins/plugin_pics.waifu.js
+++ src/commands/plugins/plugin_pics.waifu.js
@@ -1,15 +1,15 @@
-import {axiosUtils} from "../../utils/axios_Utils.js";
-import { config } from '../../config.js'
-import randomUtils from "../../utils/category_Utils.js";
+import { config } from '../../config.js';
+import randomUtils from "../../utils/randomcategory_Utils.js";
 import categoryArray from '../../helper/categoryPics.js';
+import { fetchGet_Utils } from '../../utils/fetch_Utils.js';
 
 export default async function picsPlugin (msg,conn,category) {
 
     if(!category) category = randomUtils(categoryArray);
-    console.log(category)
-    let data = await axiosUtils(`https://api.waifu.pics/sfw/${category}`);
+    
+    let data = await fetchGet_Utils(`https://api.waifu.pics/sfw/${category}`);
 
-    if(data.data.message) return await msg.reply('Não achei a categoria que sugeriu.');
+    if(data.message) return await msg.reply('Não achei a categoria que sugeriu.');
 
     const buttons = [
         {buttonId: '!menu', buttonText: {displayText: 'MENU'}, type: 1},
@@ -18,7 +18,7 @@ export default async function picsPlugin (msg,conn,category) {
       ]
       
       const buttonMessage = {
-          image: {url: data.data.url },
+          image: {url: data.url },
           caption: `*A Categoria usada foi ${category}, Se deseja usar a mesma categoria que esta aperte no botão 'SELF', para voltar a escolha de pics aleatória aperte RANDOM*`,
           footer: config.footer,
           buttons: buttons,
