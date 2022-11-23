36046f66ae4888b547815351b602fd6a6277d5c4
danzok <loppesofc@gmail.com>
2022-11-22 22:18:14 -0300

    aternando para a lib fetch

--- src/commands/plugins/plugin_waifu.js
+++ src/commands/plugins/plugin_waifu.js
@@ -1,24 +1,30 @@
-import {axiosUtils} from '../../utils/axios_Utils.js';
 import {config} from '../../config.js';
+import randomUtils from '../../utils/randomcategory_Utils.js';
+import categoryWaifu from '../../helper/categoryWaifu.js';
+import { fetchGet_Utils } from '../../utils/fetch_Utils.js'
+
 
 
 export default async function waifuPlugin (msg,conn) {
 
-    let data = await axiosUtils(`https://api.waifu.pics/sfw/waifu`);
+    
+    let category = randomUtils(categoryWaifu);
+
+    let data = await fetchGet_Utils(`https://api.waifu.im/random/?selected_tags=${category}`);
 
     const buttons = [
         {buttonId: '!menu', buttonText: {displayText: 'MENU'}, type: 1},
         {buttonId: '!waifu', buttonText: {displayText: 'PROXIMA'}, type: 1}
       ]
-      
+
       const buttonMessage = {
-          image: {url: data.data.url },
+          image: {url: data.images[0].url },
           caption: "Quer mais imagens ?",
           footer: config.footer,
           buttons: buttons,
           headerType: 4
       }
 
-      return await conn.sendMessage(msg.from, buttonMessage, { quoted : msg })
-      
+     return await conn.sendMessage(msg.from, buttonMessage, { quoted : msg })
+
 } 
