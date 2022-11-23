4f294fc085695db3a0a34802bd424deb8ec095a5
danzok <loppesofc@gmail.com>
2022-11-22 22:17:46 -0300

    adicionando comando de search image anime , com o trace-moe

--- src/commands/plugins/plugin_searchAnime.js
+++ src/commands/plugins/plugin_searchAnime.js
@@ -1,5 +1,4 @@
-//import { axiosUtilsPost } from "../../utils/axios_Utils.js";
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -10,15 +9,24 @@ const __dirname = path.dirname(__filename);
 export default async function searchPlugin (msg, conn) {
     let buffer = await msg.download(msg);
      fs.writeFileSync('./src/commands/plugins/demo.jpg', buffer)
-    let res = await fetch('https://api.trace.moe/search', {
+    let data = await fetch('https://api.trace.moe/search', {
         method: "POST",
         body: fs.readFileSync(__dirname+"/demo.jpg"),
         headers: { "Content-type": "image/jpeg" }
     })
-    
-    let result = await res.json()
-    console.log(result)
-   let messageResult =  `RESULTADO ENCONTRADO!\n\nNOME : ${result.result[0].filename}\n\nEPISODIO : ${result.result[0].episode} \n\nSEMELHANÇA : ${result.result[0].similarity}`
 
-   return await msg.reply(messageResult)
+    let res = await data.json();
+
+    //fs.unlinkSync(__dirname+"/demo.jpg");
+
+   let messageResult =  `*RESULTADO*\n\n*NOME*: ${res.result[0].filename}\n*EPISODIO* : ${res.result[0].episode} \n*SEMELHANÇA* : ${res.result[0].similarity}`
+
+  return await conn.sendMessage(
+    msg.from, 
+    { 
+        video: { url : `${res.result[0].video}` }, 
+        caption: messageResult,
+        gifPlayback: true
+    }
+     )
 }
