7692117c43dadcb57555451e6d9ceb87b3e32f87
danzok <loppesofc@gmail.com>
2022-11-22 22:18:50 -0300

    aternando para outra api de waifu com mais categorias e mais fotos

---
+++ src/helper/categoryWaifu.js
@@ -0,0 +1,11 @@
+let categoryWaifu = new Array("maid",
+    "waifu",
+    "marin-kitagawa",
+    "mori-calliope",
+    "raiden-shogun",
+    "oppai",
+    "selfies",
+    "uniform",)
+
+
+    export default categoryWaifu
