65685b2c64f174f2443f2608dd9236bd3018df16
danzok <loppesofc@gmail.com>
2022-11-22 22:16:23 -0300

    alternando para lib fetch

--- src/commands/plugins/plugin_fatos.js
+++ src/commands/plugins/plugin_fatos.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import { fetchGet_Utils } from '../../utils/fetch_Utils.js';
 import { config } from "../../config.js";
 
 
 export default async function fatosPlugin (msg, conn) {
-    let { data } = await axios.get('https://apis-geek.vercel.app/fatosRandom');
+    let { data } = await fetchGet_Utils('https://apis-geek.vercel.app/fatosRandom');
     let buttons = [
         { quickReplyButton: { displayText: "MENU ☰ ", id: `!menu` } },
         { quickReplyButton: { displayText: "PRÓXIMO ➡️", id: `!fatos` } }
